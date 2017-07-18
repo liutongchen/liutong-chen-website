c725d57f4d3d5dfaf25378975bee84193076ea47
Liutong Chen <liutong.clt@gmail.com>
2017-07-18 13:53:17 -0700

    skeleton completed

--- src/components/Contact/ContactPage.js
+++ src/components/Contact/ContactPage.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import {Col, Row} from 'react-bootstrap';
+import personalInfo from '../../../src/info';
 
 const ContactPage = () => {
     return (
         <div>
             <h2 id="contactPageTitle">Let's get in touch!</h2>
             <Row id="contactIcon">
-                <Col md={2} mdOffset={3}><img src="http://res.cloudinary.com/dla0ocfbw/image/upload/v1500355125/personalWebsite/email-icon.ico" alt="email"/></Col>
-                <Col md={2}><img src="http://res.cloudinary.com/dla0ocfbw/image/upload/v1500355124/personalWebsite/linkedin-icon.png" alt="linkedin"/></Col>
-                <Col md={2}><img src="http://res.cloudinary.com/dla0ocfbw/image/upload/v1500353959/personalWebsite/github-icon.png" alt="github"/></Col>
-            </Row>
-            <Row id="contactCategories">
-                <Col md={2} mdOffset={3}>Email</Col>
-                <Col md={2}>Linkedin</Col>
-                <Col md={2}>Github</Col>
+                <Col md={2} mdOffset={3}>
+                    <a href={"mailto:"+personalInfo.basicInfo.email}>
+                        <img src="http://res.cloudinary.com/dla0ocfbw/image/upload/v1500355125/personalWebsite/email-icon.ico" alt="email"/>
+                    </a>
+                </Col>
+                <Col md={2}>
+                    <a href={personalInfo.basicInfo.linkedin}>
+                        <img src="http://res.cloudinary.com/dla0ocfbw/image/upload/v1500355124/personalWebsite/linkedin-icon.png" alt="linkedin"/>
+                    </a>
+                </Col>
+                <Col md={2}>
+                    <a href={personalInfo.basicInfo.github}>
+                        <img src="http://res.cloudinary.com/dla0ocfbw/image/upload/v1500353959/personalWebsite/github-icon.png" alt="github"/>
+                    </a>
+                </Col>
             </Row>
         </div>
     )
