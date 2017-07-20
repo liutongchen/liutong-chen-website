9681f256b0a9aa51271a7b3491a4914ca5f1adfc
Liutong Chen <liutong.clt@gmail.com>
2017-07-19 17:28:36 -0700

    color settled

--- src/components/Projects/projectComponents.js
+++ src/components/Projects/projectComponents.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Button} from 'react-bootstrap';
+import {Button, Col, Row} from 'react-bootstrap';
 
 const ProjectComponents = ({projectsList}) => {
     const projectsElements = projectsList.map((project) => {
         return (
-            <div className="projects" key={project.title}>
+            <div className="projects" key={project.title} id={project.title.toLowerCase().split(" ").join("")}>
                 <img src={project.image} alt="Image of project" />
                 <h3>{project.title}</h3>
                 <p className="projectTech">{project.tech.join(", ")}</p>
-                <p className="projectInfo">{project.projectInfo}</p>
+                <Row><Col mdOffset={3} md={6} className="projectInfo">{project.projectInfo}</Col></Row>
+                <br/>
                 <a href={project.link} target="_blank"><Button bsStyle="primary">Learn More</Button></a>
             </div>
         );
