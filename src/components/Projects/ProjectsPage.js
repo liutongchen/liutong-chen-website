43a96458a240730ca8e39229482bf01af4601d17
Liutong Chen <liutong.clt@gmail.com>
2017-07-21 14:55:04 -0700

    solve project page introduction padding problem

--- src/components/Projects/ProjectsPage.js
+++ src/components/Projects/ProjectsPage.js
@@ -17,7 +17,7 @@ class ProjectsPage extends React.Component {
                         alt="Image" 
                         className="img-circle"
                         id="projectIntroImg"/>
-                    <Col mdOffset={4} md={4}>
+                    <Col xs={8} xsOffset={2} sm={8} smOffset={2} mdOffset={4} md={4}>
                         <h2>What I've Done</h2>
                         <p>Here's some of the work I have done so far. To learn more about the projects I have done, please go to my <a href={personalInfo.basicInfo.github}>github</a>.</p>
                     </Col>
